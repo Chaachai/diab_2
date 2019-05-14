10bc20cf24bf9925fd216b7d6197e7e47f8d0da5
farah <farah@DESKTOP-CD7JC0O>
2019-05-14 15:51:58 +0000

    Merge origin/master
    Conflicts:
    	src/GestionNote/FXML.fxml

--- src/view/ViewLauncher.java
+++ src/view/ViewLauncher.java
@@ -19,7 +19,7 @@ public class ViewLauncher extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Accueil.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Carnet.fxml"));
         
         Scene scene = new Scene(root);
         
